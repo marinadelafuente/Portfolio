d2cfd01b4f1df81da3bdb17bcda384208ca90be9
marinadelafuente <mdelafuente18@gmail.com>
2020-06-13 22:45:13 +0800

    update docs

---
+++ docs/precache-manifest.df7be95c54a8782d780c032edd40c1dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a31acd9d31a6692e404c5cff17b2b95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a738c6d402f87f0b358e",
+    "url": "./static/css/main.3c16876a.chunk.css"
+  },
+  {
+    "revision": "eae34113c9f54206e18d",
+    "url": "./static/js/2.15a72e62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.15a72e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a738c6d402f87f0b358e",
+    "url": "./static/js/main.5eebc49a.chunk.js"
+  },
+  {
+    "revision": "18c309d16c6c37590b3f",
+    "url": "./static/js/runtime-main.3207836a.js"
+  },
+  {
+    "revision": "0f6f24f2c9858caf641c02b20d4453b2",
+    "url": "./static/media/avatar-happy.0f6f24f2.png"
+  },
+  {
+    "revision": "5ecc7c1cb5231e61d01dc59c119b4ac9",
+    "url": "./static/media/avatar.5ecc7c1c.png"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "./static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "7734f3b768957944f0e5f21c4b7691e6",
+    "url": "./static/media/pattern1.7734f3b7.png"
+  },
+  {
+    "revision": "59827462fb4bd90f6a3b561da1367b0d",
+    "url": "./static/media/pattern3.59827462.png"
+  }
+]);
